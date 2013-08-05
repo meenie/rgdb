695c107112060cf931493cbab6324eb763053d93
Cody Lundquist <cody@4mation.com.au>
2013-08-05 17:46:57 +1000

    Added some GIF loading animations.

--- src/common/directives/previewGif.js
+++ src/common/directives/previewGif.js
@@ -1,28 +1,41 @@
 angular.module('previewGif', [])
     .directive('previewGif', function() {
         return {
+            restrict: 'E',
             template: '<div class="gif-preview">' +
                 '<i class="glyphicon glyphicon-play-circle"></i>' +
-                '<img ng-src="{{url}}">' +
+                '<img ng-src="{{url}}" title="{{title}}">' +
                 '</div>',
             replace: true,
             link: function(scope, element, attrs) {
-                var url = scope.$eval(attrs.previewGif),
+                var url = scope.$eval(attrs.url),
+                    title = scope.$eval(attrs.title),
                     previewUrl = 'http://gifpreview.com?url=',
-                    on = false;
+                    on = false,
+                    $img = element.find('img');
 
                 scope.url = previewUrl + url;
-                element.bind('click', function() {
-                    if (! on) {
-                        element.addClass('playing');
-                        scope.url = url;
-                    } else {
-                        element.removeClass('playing');
-                        scope.url = previewUrl + url;
-                    }
-                    on = ! on;
+                scope.title = title;
+                element.attr('title', title);
 
-                    scope.$digest();
+                element.bind('click', function() {
+                    scope.$apply(function(){
+                        if (! on) {
+                            element.addClass('spin');
+                            scope.url = url;
+                            $img.on('load', function() {
+                                $img.off('load');
+                                element.addClass('playing');
+                                element.removeClass('spin');
+                            });
+                        } else {
+                            element.removeClass('spin');
+                            $img.off('load');
+                            element.removeClass('playing');
+                            scope.url = previewUrl + url;
+                        }
+                        on = ! on;
+                    });
                 });
             }
         };
